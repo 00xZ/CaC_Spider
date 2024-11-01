6954161ba8ff8100ab193e0b2db7a75747bd90eb
Eyezik <eyezikbra@gmail.com>
2024-11-01 02:02:11 -0500

    Update worker.py
    Added Auth key

--- worker.py
+++ worker.py
@@ -1,16 +1,20 @@
-import requests ###Run this on a different computer, it will talk to the C&C
+import requests
 import json
 
 # Define the server URL
 server_url = 'http://127.0.0.1:4420'  # Replace with your server's IP
-
+AUTH_KEY = "your_secret_key"  
 # File containing scan data
 DATA_FILE = "db.json"
 
 # Function to send scan data to the server
 def send_scan_data(scan_data):
     try:
-        response = requests.post(server_url, json=scan_data)
+        headers = {
+            'Authorization': AUTH_KEY,
+            'Content-Type': 'application/json'
+        }
+        response = requests.post(server_url, json=scan_data, headers=headers)
         print(response.text)
     except requests.RequestException as e:
         print("Error sending data:", e)
